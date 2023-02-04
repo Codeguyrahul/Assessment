14ad520a61f9ee0cd662d48c35a4ed143fb38734
Codeguyrahul <122788707+Codeguyrahul@users.noreply.github.com>
2023-02-04 23:51:10 +0530

    Add files via upload

---
+++ Expression1.java
@@ -0,0 +1,18 @@
+package OBJECT;
+
+public class Expression1 {
+public static void main(String[] args) {
+	float a=101,b=0,c=3;
+	float d=3.0e;
+double e = 10000000.1;double t2=0;
+	float total=0,
+	total=(a+b)/c;
+	t2=d-6*e;
+	
+	
+	
+	
+	
+	
+}
+}
