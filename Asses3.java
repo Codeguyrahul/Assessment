4c644221b8a8afe3363ce0619b67af4d9d3be31f
Codeguyrahul <122788707+Codeguyrahul@users.noreply.github.com>
2023-02-08 17:07:50 +0530

    Add files via upload

---
+++ Asses3.java
@@ -0,0 +1,18 @@
+package method;
+import java.util.*;
+public class Asses3 {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the first number:");
+	double num1=sc.nextDouble();
+	System.out.println("enter the second number:");
+	double num2=sc.nextDouble();
+	
+	System.out.println((num1>0 && num1<1)&&(num2>0 && num2<1));
+	
+	
+	
+	
+
+	}
+}
---
+++ factorial.java
@@ -0,0 +1,15 @@
+package method;
+import java.util.Scanner;
+public class factorial {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int fact=1;
+	System.out.println("enter the number :");
+	int n=sc.nextInt();
+	for(int i=1;i<=n;i++) {
+		fact=fact*i;
+		
+	}
+	System.out.println("factorial of:"+ n +"is"+ fact);
+}
+}
