4a2720cf2d44ac90803b706c95ceb773b0316a83
Codeguyrahul <122788707+Codeguyrahul@users.noreply.github.com>
2023-02-15 16:31:54 +0530

    Add files via upload

---
+++ MyProgram.java
@@ -0,0 +1,16 @@
+
+
+class DisplayMessage{
+  void printMessage(){
+   System.out.println("Hello My Message");
+}
+}  
+
+
+public class  MyProgram
+  {
+    public static void main(String []args){
+    DisplayMessage  program=new DisplayMessage();
+      program.printMessage();
+   }
+}
